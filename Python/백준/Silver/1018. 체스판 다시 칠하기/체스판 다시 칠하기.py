0a6651e6b14577cac36b4b87a07848692272e508
codml <108252974+codml@users.noreply.github.com>
2026-03-18 17:52:41 +0900

    [Silver III] Title: 체스판 다시 칠하기, Time: 68 ms, Memory: 32544 KB -BaekjoonHub

--- Python/백준/Silver/1018. 체스판 다시 칠하기/체스판 다시 칠하기.py
+++ Python/백준/Silver/1018. 체스판 다시 칠하기/체스판 다시 칠하기.py
@@ -1,27 +1,28 @@
 import sys; input = sys.stdin.readline
 
+## 데이터 입력
 n, m = map(int, input().split())
-
 brd = []
 for row in range(n):
-    brd.append([*input().split()[0]])
+    brd.append(input()) # 문자열도 배열처럼 조회 가능
 
-cnt = 64
+min_chg = 64 # 최소 변경 횟수를 저장할 변수
+## 전체 판을 순회
 for row in range(n - 8 + 1):
     for col in range(m - 8 + 1):
-        bcnt = 0
-        wcnt = 0
+        b_chg = 0 # B가 좌상단일 때 다시 칠해야 할 칸
+        w_chg = 0 # W가 좌상단일 때 다시 칠해야 할 칸
         for i in range(8):
             for j in range(8):
                 if not (i+j) % 2:
                     if brd[row+i][col+j] != 'B':
-                        bcnt += 1
+                        b_chg += 1
                     else:
-                        wcnt += 1
+                        w_chg += 1
                 else:
                     if brd[row+i][col+j] != 'W':
-                        bcnt += 1
+                        b_chg += 1
                     else:
-                        wcnt += 1
-        cnt = min(cnt, min(bcnt, wcnt))
-print(cnt)
+                        w_chg += 1
+        min_chg = min(min_chg, min(b_chg, w_chg)) # 기존 최소 횟수와 비교
+print(min_chg)
